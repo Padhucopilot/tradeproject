1aaaaf4f2800459af875dc4160007dc6fe61069d
Padmanaban Balasubramanian <padmanabanbalasubramanian@Padmanabans-MacBook-Air.local>
2024-07-11 16:48:00 -0400

    trade usecase

--- trade2/src/main/java/com/bank/trade/repository/TradeDetail.java
+++ trade2/src/main/java/com/bank/trade/repository/TradeDetail.java
@@ -1,17 +1,12 @@
 package com.bank.trade.repository;
 
-import com.fasterxml.jackson.core.Version;
-import com.fasterxml.jackson.databind.Module;
 import jakarta.persistence.*;
 
-import jdk.jfr.DataAmount;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
-import java.util.Date;
 
 
 @Entity
--- trade2/src/main/java/com/bank/trade/repository/TradeRepository.java
+++ trade2/src/main/java/com/bank/trade/repository/TradeRepository.java
@@ -3,7 +3,9 @@ package com.bank.trade.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TradeRepository extends JpaRepository<TradeDetail, Long> {
 
 
